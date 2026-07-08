395201de03876b8236e919c52474239f3645db59
sixdgt <c4crypt@gmail.com>
2026-07-08 12:22:52 +0545

    feat: product CRUD

--- project_ecommerce/app_products/urls.py
+++ project_ecommerce/app_products/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from app_products import views
 
 urlpatterns = [
-    path('category/create/', views.product_category_create, name='category.create')
+    path('category/create/', views.product_category_create, name='category.create'),
+    # for product create and list
+    path('create/', views.product_create, name='product.create'),
+    path('list/', views.product_index, name='product.index'),
+    # for select single product, edit and delete product we need product id and it must be pass via url
+    path('view/<int:pk>/', views.product_view, name='product.view'),
+    path('edit/<int:pk>/', views.product_edit, name='product.edit'),
+    path('delete/<int:pk>/', views.product_delete, name='product.delete'),
 ]
--- project_ecommerce/app_products/views.py
+++ project_ecommerce/app_products/views.py
@@ -17,3 +17,78 @@ def product_category_create(request):
         "category_form": category_form
     }
     return render(request, "categories/category_create.html", context)
+
+def product_create(request):
+    """
+    This view is for creating a new product in the database. It handles both GET and POST requests.
+    GET: Renders the product creation form.
+    POST: Validates and saves the product data to the database.
+    """
+    if request.method == "POST":
+        request_data = request.POST
+        form_data = ProductForm(request_data)
+
+        if form_data.is_valid():
+            form_data.save()
+            return redirect("product.create")
+    else:
+        product_form = ProductForm()
+        context = {
+            "product_form": product_form,
+            "title": "Create Product",
+            "button_text": "Create Product"
+        }
+        return render(request, "products/product_form.html", context)
+
+def product_index(request):
+    """
+    This view is responsible for displaying all the products in the database.
+    """
+    products = Product.objects.all() # similar to SELECT * FROM products in SQL
+    context = {
+        "products": products
+    }
+    return render(request, "products/product_index.html", context)
+
+def product_view(request, pk):
+    """
+    This view is responsible for displaying a single product based on its primary key (pk).
+    """
+    product = Product.objects.get(pk=pk) # similar to SELECT * FROM products WHERE id = pk in SQL
+    context = {
+        "product": product
+    }
+    return render(request, "products/product_view.html", context)
+
+def product_edit(request, pk):
+    """
+    This view is responsible for editing an existing product based on its primary key (pk).
+    GET: Renders the product edit form with existing data.
+    POST: Validates and updates the product data in the database.
+    """
+    product = Product.objects.get(pk=pk)
+    if request.method == "POST":
+        request_data = request.POST
+        form_data = ProductForm(request_data, instance=product)
+
+        if form_data.is_valid():
+            form_data.save()
+            return redirect("product.index")
+    else:
+        # re-using product_form.html template for product edit form
+        product_form = ProductForm(instance=product)
+        context = {
+            "product_form": product_form,
+            "product": product,
+            "title": "Edit Product",
+            "button_text": "Update Product"
+        }
+        return render(request, "products/product_form.html", context)
+
+def product_delete(request, pk):
+    """
+    This view is responsible for deleting an existing product based on its primary key (pk).
+    """
+    product = Product.objects.get(pk=pk)
+    product.delete()
+    return redirect("product.index")
